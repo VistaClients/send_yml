41394769ebcf024e81de1024f0826c3194fbe118
VistaClients <vista.clients@rapttechnologies.com>
2025-11-28 17:53:41 +0530

    Update send_email.js

--- assets/js/common_js/send_email.js
+++ assets/js/common_js/send_email.js
@@ -1,43 +1,40 @@
+async function fetch_token() {
+  try {
+    const response = await fetch('send_email_token.json');
 
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
-async function fetch_token(){
-   fetch('send_email_token.json')
-    .then(response => {
-        if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const first_token = data.first_token;
-        const second_token = data.second_token;
-        console.log("Token:", token); 
-            return first_token+second_token;
-  
-    })
-    .catch(error => {
-        console.error("Failed to fetch token:", error);
-            return null;
-  
-    });
-  
+    const data = await response.json();
+    return data.first_token + data.second_token;
+
+  } catch (error) {
+    console.error("Failed to fetch token:", error);
+    return null;
   }
-  
+}
 
-window.sendEmailDataToGitHub = async function (emailSubject, emailBody) {
-  const secret  = await fetch_token();
+window.sendEmailDataToGitHub = async function(emailSubject, emailBody) {
+  const token = await fetch_token();
+
+  if (!token) {
+    console.error("Token missing or failed to load.");
+    return;
+  }
+
   const response = await fetch('https://api.github.com/repos/VistaClients/send_yml/dispatches', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `token ${secret}`,  
+      'Authorization': `token ${token}`
     },
     body: JSON.stringify({
-      event_type: 'send_email', 
+      event_type: 'send_email',
       client_payload: {
         subject: "Test Subject",
-        to_email:'fashionsense482@gmail.com',
-        body: "This is the body of the email.",
+        to_email: 'fashionsense482@gmail.com',
+        body: "This is the body of the email."
       }
     })
   });
@@ -48,4 +45,3 @@ window.sendEmailDataToGitHub = async function (emailSubject, emailBody) {
     console.error('Error triggering GitHub Actions');
   }
 };
-
